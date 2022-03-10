ff5058f053e60f78eb65cb6b59ec925f95c11701
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-03-10 14:52:22 -0600

    adding middleware

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ require("dotenv").config();
 const PORT = process.env.PORT || 9000;
 
 const express = require("express");
+const cors = require("cors");
 
 const server = express();
 
+server.use(express.json());
+server.use(cors());
+
+server.get("/api/hello", (req, res) => {
+  res.json({
+    message: "api is working",
+  });
+});
+
 server.get("/", (req, res) => {
   res.end("<h1>Hi</h1>");
+});
+
+server.use((err, req, res, next) => {
+  //eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
 });
 
 server.listen(PORT, () => {
