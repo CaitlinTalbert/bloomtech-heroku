1c59ef03f51a7f146e74605aef49ef67125ea4cb
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-03-10 11:58:10 -0600

    use env.port

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+const PORT = process.env.PORT || 9000;
+
 const express = require("express");
 
 const server = express();
@@ -6,6 +10,9 @@ server.get("/", (req, res) => {
   res.end("<h1>Hi</h1>");
 });
 
-server.listen(9000, () => {
-  console.log("server is up and working on port 9000");
+server.listen(PORT, () => {
+  console.log(process.env.MY_ENV);
+  console.log(
+    `server is up and working on port ${PORT}, user is ${process.env.USER}`
+  );
 });
